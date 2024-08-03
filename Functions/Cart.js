144f90311ea6332ffbd100029114a8549321dddb
Y_Leela_Krishna <y.leelakrishna572@gmail.com>
2024-08-03 22:36:12 +0530

    work on delete button and save data to local storage

--- Functions/Cart.js
+++ Functions/Cart.js
@@ -1,19 +1,48 @@
-const cart = [];
+let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+    cart = [{
+        Id : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity : 2
+    },{
+        Id : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity : 1
+    }];
+}
+
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 function addtocart(itemId){
     let existingItem;
-        cart.forEach((item) => {
-            if(item.Id === itemId){
-                existingItem = item;
-            }
-        })
-
-        if(existingItem){
-            existingItem.quantity += 1;
-        }else{
-            cart.push({
-                Id : itemId,
-                quantity : 1
-            })
+    cart.forEach((item) => {
+        if(item.Id === itemId){
+            existingItem = item;
         }
+    })
+
+    if(existingItem){
+        existingItem.quantity += 1;
+    }else{
+        cart.push({
+            Id : itemId,
+            quantity : 1
+        })
+    }
+    saveToStorage();
+}
+
+function deleteFromCart(itemid){
+    let newCart = [];
+    
+    cart.forEach((cartItem) => {
+        if(cartItem.Id != itemid){
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+    saveToStorage();
 }
---
+++ Functions/Checkout.js
@@ -0,0 +1,102 @@
+
+let checkoutHtml = '';
+
+
+cart.forEach((cartItem) =>{
+    let matchingItem;
+
+    items.forEach((item) => {
+        if(item.id === cartItem.Id){
+            matchingItem = item;
+        }
+    })
+
+    checkoutHtml += `<div class="cart-item-container container-js-${matchingItem.id}">
+            <div class="delivery-date">
+              Delivery date: Tuesday, June 21
+            </div>
+
+            <div class="cart-item-details-grid">
+              <img class="product-image"
+                src="${matchingItem.image}">
+
+              <div class="cart-item-details">
+                <div class="product-name">
+                  ${matchingItem.name}
+                </div>
+                <div class="product-price">
+                  &#8377;${((matchingItem.priceCents/100)*80).toFixed(2)}
+                </div>
+                <div class="product-quantity">
+                  <span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                  </span>
+                  <span class="update-quantity-link link-primary">
+                    Update
+                  </span>
+                  <span class="delete-quantity-link link-primary delete-item-js" data-item-id="${matchingItem.id}">
+                    Delete
+                  </span>
+                </div>
+              </div>
+
+              <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" checked
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingItem.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Tuesday, June 21
+                    </div>
+                    <div class="delivery-option-price">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingItem.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Wednesday, June 15
+                    </div>
+                    <div class="delivery-option-price">
+                      $4.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingItem.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Monday, June 13
+                    </div>
+                    <div class="delivery-option-price">
+                      $9.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+});
+
+
+document.querySelector('.summary-js').innerHTML = checkoutHtml;
+
+document.querySelectorAll('.delete-item-js').forEach((item) => {
+    const itemId = item.dataset.itemId;
+    item.addEventListener('click', () =>{
+        deleteFromCart(itemId);
+        
+        const containerEl = document.querySelector(`.container-js-${itemId}`);
+        containerEl.remove();
+    })
+})
